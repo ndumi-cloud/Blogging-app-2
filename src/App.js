78eb156b1df5d20e856cc86ca5fcb58975a51ca0
gracendumi <ndumigrace71@gmail.com>
2024-05-27 17:28:41 +0300

    final work

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (       
     <div>
-      <BrowserRouter basename="/blog11">
+      <BrowserRouter basename="/Blogging-app-2">
         <TopBar setSearchQuery={setSearchQuery} />
         <Routes>
           <Route path="/" element={<Homepage searchQuery={searchQuery} />} />
